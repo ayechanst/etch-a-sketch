74ee41de921d0819b4fdd7c3d6db3ce06f7ddb96
Aye Chan <ayechan.sant@gmail.com>
2023-01-27 23:00:14 -0500

    make grid

--- etchASketch.js
+++ etchASketch.js
@@ -1,1 +1,21 @@
-console.log('wassup')
+"use strict";
+
+document.body.onload = makeDivs;
+
+const divContainer = document.getElementById('wrapper');
+const tile = document.querySelector('.tile');
+
+
+function makeDivs() {
+    for (let i = 0; i < 256; i ++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('tile');
+        let newContent = document.createTextNode("");
+        newDiv.appendChild(newContent);
+        divContainer.appendChild(newDiv);
+    }
+}
+
+tile.addEventListener("mouseover", function() {
+    tile.className = 'activated';
+})
