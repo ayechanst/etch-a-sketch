a735021e3e469e9e6ee0a292f37c4e895207504b
Aye Chan <ayechan.sant@gmail.com>
2023-01-30 20:08:16 -0500

    make grid adjustable

--- etchASketch.js
+++ etchASketch.js
@@ -1,49 +1,43 @@
 "use strict";
 
-const divContainer = document.getElementById('wrapper');
+const divContainer = document.getElementById('container');
 const button = document.getElementById('btn');
 
 //helper functions
 
-// function deleteGrid() {
-//     tiles.forEach((tile) => {
-//         tile.remove();
-//     })                                 
-// }
+function deleteGrid() {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach((tile) => {
+        tile.remove();
+    })                                 
+}
 
 function makeGrid(gridLength) {
-    // let newDiv = document.createElement('div');
-    // newDiv.classList.add('tile');
-    // for (let i = 0; i < gridLength; i++) {
-    // }
-    
-
-    let gridSize = gridLength ** 2;
-    for (let i = 0; i < gridSize; i ++) {                 //convert to square
+    deleteGrid();
+    const container = document.getElementById('container');
+    let numOfSquares = gridLength * gridLength;
+    for (let i = 0; i < numOfSquares; i ++) {                 
         let newDiv = document.createElement('div');
         newDiv.classList.add('tile');
-        let newContent = document.createTextNode('');
+        let newContent = document.createTextNode('');      
         newDiv.appendChild(newContent);
         divContainer.appendChild(newDiv);
-        console.log()
     }
+    container.style.gridTemplateColumns = repeat(`${gridLength}`, `${1}px`);
 }
-const tiles = document.querySelectorAll('.tile');           //figure out why the node list is empty, this will help solve
-                                                        //filling the grid in and also resetting the grid
+makeGrid(6);
+const tiles = document.querySelectorAll('.tile');   
 
 //end helper functions
 
 console.log(tiles);
-makeGrid(16);
 
 
 
 button.addEventListener('click', function askGrid() {        //function that asks grid size and deletes old
-    //deleteGrid();
     let gridLength = prompt('How many squares long should the square grid be?');
     if (gridLength > 100) {
-        gridLength = 100;
-        return makeGrid(gridLength);
+        return makeGrid(100);
     }
     else {
         return makeGrid(gridLength);
@@ -53,7 +47,7 @@ button.addEventListener('click', function askGrid() {        //function that ask
 
 tiles.forEach((tile) => {
     tile.addEventListener("mouseover", function(e) {
-        tile.style.background = 'red';
+        tile.style.background = 'blue';
         console.log(e);
     })
 })
@@ -62,3 +56,9 @@ tiles.forEach((tile) => {
 //make grid area stay the same size
 //make grid retain color
 //makes grid delete itself when making a new grid
+
+
+
+
+
+//  IIIII ``
